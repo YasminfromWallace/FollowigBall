be5db85419ca0b13849a372cd66c6760f4f2048c
Y𖤐smin <yxavieroliveira@gmail.com>
2024-11-28 09:15:02 -0300

    Save 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FollowigBall
 Meu segundo projeto (Ainda em época de testes) / My second ever project! (I'm still testing things)
+
+Para esse projeto eu precisava fazer um código em que a forma seguisse meu mouse a todo momento.
+Eu comecei escolhendo o círculo pois seu ponto de centro é bem no meio, sendo melhor visualmente;
---
+++ sketch.js
@@ -0,0 +1,30 @@
+let cor;
+let x;
+let y;
+
+function setup() {
+ createCanvas(1250, 700);
+ background(random(0, 150), (0, 150), (0, 150));
+ 
+ 
+ cor = color(random(150, 255), random(150 ,255), random(150, 255));
+ x = (random(0, 400));
+ y = (random(0, 400));
+}
+
+function draw() {
+circle (x, y, 50);
+ fill (cor);
+ 
+ if(mouseX < x) {
+   x = x - 1}
+ if(mouseX > x) {
+   x = x + 1 }
+ if(mouseY < y) {
+   y = y - 1}
+ if(mouseY > y) {
+   y = y + 1}
+
+ if(mouseIsPressed) {
+   cor = color(random(150, 255), random(150 ,255), random(150, 255)) }
+}
